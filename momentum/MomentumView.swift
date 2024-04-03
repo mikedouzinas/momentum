3f08fd6cfb9aef19e8d907c768645ee5742b8922
milobaron <mikeveson@icloud.com>
2024-04-03 14:50:31 -0500

    Altered some UI, and added a Textview for transcription

--- momentum/MomentumView.swift
+++ momentum/MomentumView.swift
@@ -1,72 +1,107 @@
 import SwiftUI
-import AVFoundation // Import AVFoundation for handling audio recording
+import AVFoundation
+import Speech
 
 struct MomentumView: View {
     @State private var isRecording = false
-    @State private var audioText = "Start speaking..."
-    // Add more state variables as needed for handling timer, editing, etc.
+    @State private var audioText = "Hold button to begin speaking..."
+    @State private var endAudioText = ""
+    @ObservedObject private var speechRecognizer = SpeechRecognizer()
+    @State private var animationAmount: CGFloat = 1
+    
     
     var body: some View {
-        VStack(spacing: 20) {
-            Button(action: {
-                // Handle button press logic
-                self.startRecording()
-            }, label: {
-                // Replace "Logo" with your actual logo image
-                Image(systemName: "mic") // Placeholder for logo
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100)
-                    .background(isRecording ? Color.red : Color.blue) // Change color to indicate recording
-                    .clipShape(Circle())
-                    .foregroundColor(.white)
-            })
-            .buttonStyle(PlainButtonStyle())
-            .onLongPressGesture(minimumDuration: .infinity, pressing: { pressing in
-                self.isRecording = pressing
-                if pressing {
-                    // Start recording
-                } else {
-                    // Stop recording
-                    self.stopRecording()
-                }
-            }, perform: { })
-            
-            Text(audioText)
-                .padding()
-            
-            HStack {
-                CircleTimerView() // Placeholder for the timer view
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+
+                // Main Button centered in the middle
                 Button(action: {
-                    // Handle edit action
+                    // Action intentionally left blank
                 }) {
-                    Image(systemName: "pencil.circle.fill")
+                    Image("logo") // change logo later
                         .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.blue)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 140, height: 140)
+                        .background(isRecording ? Color.gray : Color.white)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .stroke(isRecording ? Color.red : Color.blue, lineWidth: 5)
+                                .scaleEffect(animationAmount)
+                                .opacity(Double(2 - animationAmount))
+                                .animation(isRecording ? Animation.easeInOut(duration: 1).repeatForever(autoreverses: true) : .default, value: animationAmount)
+                        )
                 }
+                .buttonStyle(PlainButtonStyle())
+                .onLongPressGesture(minimumDuration: .infinity, pressing: { pressing in
+                    self.isRecording = pressing
+                    if pressing {
+                        
+                        self.animationAmount = 2
+                        endAudioText = "Release to finish."
+                        
+                        // self.startRecording()
+                    } else {
+                        endAudioText = ""
+                        self.animationAmount = 1
+                        // Stop recording
+                        // self.stopRecording()
+                    }
+                }, perform: { })
+                .foregroundColor(.white)
+                .frame(width: geometry.size.width) // Force the button to occupy the full width of the GeometryReader
+                // Positioned text above the center
+                Text(audioText)
+                    .frame(width: geometry.size.width * 0.8, height: 50)
+                    .background(Color.gray.opacity(0.3))
+                    .cornerRadius(10)
+                    .padding(.bottom, 20)
+                    .padding(.top, 40)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.leading) // Ensure text is centered if it wraps
                 
-                Button(action: {
-                    // Handle go green action
-                }) {
-                    Image(systemName: "arrow.right.circle.fill")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(.green)
+                Text("transcription")
+                    .frame(width: geometry.size.width * 0.8, height: 200)
+                    .background(Color.gray.opacity(0.3))
+                    .cornerRadius(10)
+                    .padding(.bottom, 60)
+                    .multilineTextAlignment(.leading)
+                
+                
+                // Bottom three buttons
+                HStack(spacing: 20) { // Add spacing between buttons
+                    CircleTimerView()
+                    Button(action: {}) {
+                        Image(systemName: "pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.blue)
+                    }
+                    Button(action: {}) {
+                        Image(systemName: "arrow.right.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.green)
+                    }
                 }
+                .frame(width: geometry.size.width, height: 50, alignment: .center) // Ensure HStack is centered
             }
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure the GeometryReader takes up all available space
     }
+
     
     private func startRecording() {
-        // Start recording audio
-        // Update `audioText` with the transcribed text
+        do {
+            try speechRecognizer.startRecording()
+        } catch {
+            print("Failed to start recording: \(error)")
+        }
     }
     
     private func stopRecording() {
-        // Stop recording audio
-        // Finalize `audioText` updates
+        speechRecognizer.stopRecording()
     }
 }
 
--- momentum/SpeechRecognizer.swift
+++ momentum/SpeechRecognizer.swift
@@ -5,4 +5,65 @@
 //  Created by Mike Veson on 4/2/24.
 //
 
-import Foundation
+import SwiftUI
+import AVFoundation
+import Speech
+
+class SpeechRecognizer: ObservableObject {
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    @Published var transcribedText: String = ""
+    
+    func startRecording() throws {
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+
+        let inputNode = audioEngine.inputNode // Directly access the input node
+
+        recognitionRequest?.shouldReportPartialResults = true
+
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest!) { result, error in
+            var isFinal = false
+
+            if let result = result {
+                self.transcribedText = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+            }
+
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        }
+
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.recognitionRequest?.append(buffer)
+        }
+
+        audioEngine.prepare()
+        try audioEngine.start()
+    }
+
+    
+    func stopRecording() {
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+        try? AVAudioSession.sharedInstance().setActive(false)
+    }
+}
+
